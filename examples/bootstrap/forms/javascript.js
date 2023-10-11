c0eb52ddd6e5a56beaa26f82c9bbbfb704377751
Michael DeSanty <mdesanty6@gmail.com>
2023-10-11 13:33:14 -0400

    Bootstrap form and table examples

--- examples/bootstrap/forms/javascript.js
+++ examples/bootstrap/forms/javascript.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
+  console.log('test');
+
   const courseSelect = document.getElementById('course-select');
   courseSelect.addEventListener('change', function() {
     const defaultOption = document.getElementById('default-course-option');
@@ -7,25 +9,31 @@ document.addEventListener('DOMContentLoaded', function() {
       courseSelect.removeChild(defaultOption);
   });
 
-  const form = document.getElementById('my-form');
-  form.addEventListener('submit', handleSubmit);
+  const submitBtn = document.getElementById('submit-btn');
+  submitBtn.addEventListener('click', handleSubmit);
 });
 
 function handleSubmit(event) {
-  const form = event.target;
-
   event.preventDefault();
   event.stopPropagation();
 
-  if(!form.checkValidity()) {
-    console.log('Oops. Cannot get the data from an invalid form.');
-  }
-  else {
-    const rawData = new FormData(event.target);
-    const data = Object.fromEntries(rawData.entries());
+  // Get the values individually:
+  console.log(`Username: ${document.getElementsByName('username')[0].value}`);
+  console.log(`Email: ${document.getElementsByName('email')[0].value}`);
+  console.log(`First name: ${document.getElementsByName('firstName')[0].value}`);
+  console.log(`Last name: ${document.getElementsByName('lastName')[0].value}`);
+  console.log(`course: ${document.getElementsByName('course')[0].value}`);
+  console.log(`Love it: ${document.getElementsByName('loveThisCourse')[0].value}`);
 
-    console.log(data);
-  }
+  // There may not always be a checked radio button. If there isn't, we don't want to call .value on undefined.
+  const attendeeType = document.querySelector('input[name="attendeeType"]:checked');
+  if(attendeeType)
+    console.log(`Attendee type: ${attendeeType.value}`);
 
-  form.classList.add('was-validated');
+  // Get all of the form data as an objct:
+  const form = document.getElementById('my-form');
+  const rawData = new FormData(form);
+  const data = Object.fromEntries(rawData.entries());
+
+  console.log(data);
 }
